9146a0853213c73fbeaff95c7b7934ecd66bdc38
Vanderson <vanderson@Vandersons-iMac.local>
2022-09-09 16:24:51 -0300

    Final Project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,7 +1,26 @@
 import { Request, Response } from 'express';
+import { Pet } from '../models/Pet';
 
 export const search = (req: Request, res: Response) => {
 
-    //res.render('pages/page);
+    let query: string = req.query.q as string;
+
+    if(!query){
+        res.redirect('/');
+        return;
+    }
+
+    let list = Pet.getFromName(query);
+
+    res.render('pages/page', {
+        list,
+        search: query,
+        menu: {
+            all: true,
+            dogs: false,
+            cats: false,
+            fishes: false
+        }
+    });
 
 }
